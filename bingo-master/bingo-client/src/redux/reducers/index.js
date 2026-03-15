67bd95f6c5443c33cf30e4774cb362c698906d5c
dangochinh <dangochinh@gmail.com>
2026-03-15 14:37:24 +0700

    chore: clean up repo to only contain pe-thui

--- bingo-master/bingo-client/src/redux/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-import gameLobby from "./gameLobby";
-import bingoGame from "./bingoGame";
-import chatWindow from "./chatWindow";
-
-export default combineReducers({ chatWindow, gameLobby, bingoGame });
--- bingo-master/python-bingo-server/server/__init__.py
+++
@@ -1,2 +0,0 @@
-__all__ = ['server']
-from . import server

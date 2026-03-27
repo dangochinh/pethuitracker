a34c57bb7bf41e80977c2f57283e4e53b9554aef
dangochinh <dangochinh@gmail.com>
2026-03-27 18:45:56 +0700

    Fix PWA status bar color: change theme-color from pink to white for consistency

--- pe-thui/app/layout.js
+++ pe-thui/app/layout.js
@@ -33,7 +33,7 @@ export default function RootLayout({ children }) {
         <head>
             <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
             <link rel="manifest" href="/manifest.json" />
-            <meta name="theme-color" content="#E91E63" />
+            <meta name="theme-color" content="#FFFFFF" />
             <link rel="icon" href="/Logo.png" />
             <link rel="apple-touch-icon" sizes="180x180" href="/Logo.png" />
         </head>
